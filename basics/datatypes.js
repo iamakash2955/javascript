b34206f37f9f4104f2f0289e36d14d340b1daea5
Akash Kumar Sinha <aksinha295@gmail.com>
2024-08-17 06:28:16 +0000

    Datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js, not browser
+
+console.log (3
+    + 3
+)        
+
+console.log (3 + 3)   // code readability should be high, not a good step to write the code
+
+// for javascript documentation == TC39 and MDN, ECMA
+
+let name = "Akash"
+let age = 18
+let isLoggedIn = false
+
+/*
+Data Types:-
+number => 2^52 
+bigint
+string => ""
+boolean => true/false
+null => standalone value; type
+undefined => variable declared but value not assigned
+symbol => to find uniqueness 
+............primitive datatypes..........
+
+*/
+//object
+
+console.log(typeof age ); //number
+console.log(typeof "Akash"); // string
+console.log(typeof undefined); //undefined
+console.log(typeof null);  //object
