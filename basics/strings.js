3cdce7f5d66cc83072d5a049d0ea75567ff7c75b
Akash Kumar Sinha <aksinha295@gmail.com>
2024-08-18 19:42:38 +0000

    Comments added

--- basics/strings.js
+++ basics/strings.js
@@ -8,12 +8,12 @@ console.log(name + repoCount + " value");
 console.log(`My name is ${name} and my repo count is ${repoCount}`); //readable and reliable, better to previous one and modern; string interpolation
 
 const gameName = new String('iamakash-kumar-sinha')
-console.log(gameName[1]);
-console.log(gameName.__proto__);
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('k'));
+console.log(gameName[1]);                   // a
+console.log(gameName.__proto__);               // {}
+console.log(gameName.length);                  //20
+console.log(gameName.toUpperCase());        //IAMAKASH-KUMAR-SINHA
+console.log(gameName.charAt(2));              //m
+console.log(gameName.indexOf('k'));           //4  
 
 const newString = gameName.substring(0, 4) //it will not include the last index
 console.log(newString);
@@ -25,11 +25,11 @@ const newStringOne = "        akash"
 console.log(newStringOne);
 console.log(newStringOne.trim());          // it will remove the white space from the string
 
-const url = "https://akash.com/akash%20sinha"
-console.log(url.replace('%20', '-'))
+const url = "https://akash.com/akash%20sinha"            
+console.log(url.replace('%20', '-'))                      //https://akash.com/akash-sinha
 
-console.log(url.includes('akash'))
+console.log(url.includes('akash'))      //True
 
-console.log(gameName.split('-'))
+console.log(gameName.split('-'))        //[ 'iamakash', 'kumar', 'sinha' ]
 
 /*****************************************************8 */
